617e31050ed2c98a3b8221f99968e1277c04504f
Ahmad Sofyan .A <sofyan7273@gmail.com>
2022-04-03 16:44:36 +0700

    praktikum_modul_06

---
+++ modul-06/src/App.js
@@ -0,0 +1,14 @@
+
+import CreateTodo from './containers/CreateTodo';
+import Table from './containers/Table';
+
+function App() {
+  return (
+    <div className="container">
+      <CreateTodo/>
+      <Table/>
+    </div>
+  );
+}
+
+export default App;
